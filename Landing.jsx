7a8be12294df88690ef30ca5caf205939fb45cff
VineetPshukla <vineetshukla000777@gmail.com>
2025-02-21 22:35:35 +0530

    Add files via upload

---
+++ Landing.jsx
@@ -0,0 +1,15 @@
+
+function Landing() 
+{
+return(<>
+<div className='content'>
+          <h2> I Welcome you all to the Auction App</h2>
+          <p>
+          An online auction application is a web or mobile application that enables users to bid on items listed by other users. Online auction apps usually include two sets of users: bidders and sellers. Bidders use the app to view item listings, place bids, check their bids status, and review previously purchased items. Sellers use the app to create new listings for items they want to sell and edit existing listings. The app may also include an admin dashboard that enables administrators to monitor activity across all applications in the network. Online auction apps are similar to most other shopping apps with search features, such as price comparison apps, product review apps, and retail shopping apps. 
+          </p>
+          </div>
+          </>
+)
+}
+
+export default Landing
